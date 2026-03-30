01e242e96365e0ceb26b555e36977fd2641ee1e5
Jose Alonzo <pepemxl@gmail.com>
2026-03-29 18:48:52 -0600

    organize

--- chapter_01/codes/example_03/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("Número pi: 3.14159");
-    println!("Número pi: {}","3.14159");
-    println!("Número pi: {}",3.14159);
-}
--- chapter_01/codes/example_08/src/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("Hello, 
-    world!");
-    println!("Hello, \
-    world!");
-}
